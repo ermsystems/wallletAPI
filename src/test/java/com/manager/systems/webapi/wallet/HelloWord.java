3e8dac01a24329214576ad02084e15fcb5e679d0
Emerson Mariano <emerson@datamaxi.com.br>
2020-05-26 09:36:10 -0300

    First test class

--- src/test/java/com/manager/systems/webapi/wallet/HelloWord.java
+++ src/test/java/com/manager/systems/webapi/wallet/HelloWord.java
@@ -1,17 +1,18 @@
 package com.manager.systems.webapi.wallet;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
-class ManagerSystemsWebApiWalletApplicationTests {
+@SpringBootTest
+public class HelloWord {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
 }
