a4b4dfd25ebbf5ef02ab23ef3d59dd492ad205e7
Emerson Mariano <emerson@datamaxi.com.br>
2020-05-26 09:12:29 -0300

    Creation repository wallet

---
+++ src/test/java/com/manager/systems/webapi/wallet/ManagerSystemsWebApiWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.systems.webapi.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerSystemsWebApiWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
